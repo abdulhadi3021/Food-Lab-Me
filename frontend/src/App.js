86fb36272b72141b20c1436c1cbda5a90150f281
abdulhadi3021 <getsecondtime@gmail.com>
2025-04-17 17:53:01 +0500

    backend and fronted react.js
    backend and fronted react.js

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import 'flowbite';
+import Navbar from './components/Navbar';
+import { Routes, Route } from 'react-router-dom';
+import Food from './pages/Food';
+import Home from './pages/Home';
+import FoodDetail from './pages/FoodDetail';
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+
+      <Routes>
+        <Route path = "/create-food" element = {<Food/>}/>
+        <Route path = "/" element = {<Home/>}/>
+        <Route path = "/food-detail/:id" element = {<FoodDetail/>}/>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
